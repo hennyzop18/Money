e7346636116ca375f4be1a471e555a73b95fc955
hennyzop18 <116883947+hennyzop18@users.noreply.github.com>
2025-12-17 09:47:54 +0700

    fixx

--- train.py
+++ train.py
@@ -42,7 +42,7 @@ def build_transforms(image_size: int):
         ]
     )
     val_tf = transforms.Compose(
-        [``
+        [
             transforms.Resize((image_size, image_size)),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
